f20c75a5f4291678cf8d9560697aba49ac3cb64d
Raveendra Meesala <meesala10@gmail.com>
2022-12-12 09:01:03 +0530

    first commit

--- Hello.java
+++
@@ -1,5 +0,0 @@
-public class Ravi{
-  public static void main(String [] args){
-     System.out.println("Hwllow World");
-}
-}
---
+++ assignment15.java
@@ -0,0 +1,23 @@
+import static java.lang.System.out;
+
+public class InueronPattern {
+    public static void main(String[] args) {
+        int n = 15;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i-j >= (n-1)/2 && i >= (n-1)/2)  ||( i+j >=(n-1) +(n-1)/2 )) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+
+
+        }
+    }
+}
+
+
+
